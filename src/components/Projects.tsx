1ef74588fcdf1fb4be51248391e82753fb336e18
dhanasekar8363-lab <dhanasekar8363.com>
2026-07-15 15:39:50 +0530

    latest changes

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,13 +7,13 @@ export default function Projects() {
       id="projects"
       className="py-24 bg-[#FFF5F7]"
     >
-      <div className="max-w-7xl mx-auto px-8">
+      <div className="max-w-7xl mx-auto">
 
-        <h1 className="text-5xl font-bold text-center">
+        <h1 className="text-5xl font-bold text-center px-8">
           Featured Projects
         </h1>
 
-        <p className="text-center text-gray-500 mt-5">
+        <p className="text-center text-gray-500 mt-5 px-8">
           Some of my recent work
         </p>
 
@@ -26,11 +26,11 @@ export default function Projects() {
               overflow-x-auto
               snap-x
               snap-mandatory
-              px-4
-              lg:px-0
+              px-5
               lg:grid
               lg:grid-cols-3
               lg:gap-8
+              lg:px-8
               lg:overflow-visible
               pb-4
             "
@@ -40,10 +40,9 @@ export default function Projects() {
               <div
                 key={project.title}
                 className="
+                  shrink-0
+                  w-[82%]
                   snap-center
-                  flex-shrink-0
-                  w-[82vw]
-                  sm:w-[380px]
                   lg:w-auto
                 "
               >
